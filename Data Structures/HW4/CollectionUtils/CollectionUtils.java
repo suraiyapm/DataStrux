6dc671177b393b2aaf35f9f8fd39886586c39384
Suraiya P Mark <s.pobmark@gmail.com>
2025-03-02 17:32:13 -0500

    poosh

--- Data Structures/HW4/CollectionUtils/CollectionUtils.java
+++ Data Structures/HW4/CollectionUtils/CollectionUtils.java
@@ -1,23 +1,27 @@
 import java.util.Collection;
 import java.util.Set;
+import java.util.HashSet;
+
 public class CollectionUtils {
 
-    public double average(Collection<Integer> x) {
+    public static double average(Collection<Integer> x) {
         double sum=0;
         for (int z : x) {
             sum+=z;
         }
         return sum/(x.size());
     }
-    public int maxLength(Collection<String> x) {
+    public static int maxLength(Collection<String> x) {
         int max = 0;
         for(String z : x) {
             if (z.length()>max) max=z.length();
         }
         return max;
     }
-    public boolean containsDuplicate(Collection<Integer> x) {
-        Set<Integer> intSet = new Set<>;
-//create copy check length? :D
+    public static boolean containsDuplicate(Collection<Integer> x) {
+        Set<Integer> intSet = new HashSet<>(x);
+        //create copy check length? :D
+
+        return !(x.size()==intSet.size());
     }
 }
