78ec24f388808bb5686e7eb1148b881f49840aa3
Suraiya P Mark <s.pobmark@gmail.com>
2025-04-09 17:52:13 -0400

    poosh

--- Data Structures/ArrayDeque/ArrayDeque.java
+++ Data Structures/ArrayDeque/ArrayDeque.java
@@ -1,17 +1,38 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
-public class ArrayDeque<E> {
-    private final int DEFAULT_CAPACITY = 10;
-    int capacity;
-    int size;
+public class ArrayDeque<E> implements Iterable<E> {
+    private E[] elements;
+    private int indxOfFirst;
+    private int indxOfLast;
+    private static final int DEFAULT_CAPACITY = 10;
+
     public ArrayDeque() {
-        this.capacity = DEFAULT_CAPACITY;
+        this(DEFAULT_CAPACITY);
     }
     public ArrayDeque(int initialCapacity) {
-        this.capacity = initialCapacity;
+        elements = (E[]) new Object[initialCapacity];
+        indxOfFirst=indxOfLast=-1;
     }
-    public int size() { return size; }
-    public boolean isEmpty() { return size==0; }
+    public int size() {
+        if (indxOfFirst==-1 && indxOfLast==-1) {
+            return 0;
+        } else if (indxOfFirst<=indxOfLast) {
+            return indxOfLast-indxOfFirst+1;
+        } else {
+            return (indxOfLast+1) + (elements.length-indxOfFirst); //circular/buffer
+        }
+    }
+    public boolean isEmpty() {
+        return size()==0;
+    }
     public void addFirst(E element) {
-
+        if (size()==elements.length) {
+            E[] newArr = (E[]) new Object[2*elements.length+1];
+            newArr[0] = element;
+            for (int i=1; i<elements.length; i++) {
+                newArr[i] = elements[i-1];
+            }
+        }
     }
 }
